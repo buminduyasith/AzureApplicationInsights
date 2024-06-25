5548415b5d8a4a32f82e3405071ffe4a62d7be6f
Bumindu Yasith <bumiduyasith@gmail.com>
2024-06-25 21:44:09 +0530

    Fix linting issues and ensure functionality

--- AzureApplicationInsights/Program.cs
+++ AzureApplicationInsights/Program.cs
@@ -5,7 +5,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
